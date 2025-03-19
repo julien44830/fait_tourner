1f94f0627a17ffe67dbf426b9fa88677fd300ea5
julien44830 <deniaudjulien0@gmail.com>
2025-03-19 16:07:15 +0100

    add MPD on project & map on requete book for display picture

--- client/src/page/Book.tsx
+++ client/src/page/Book.tsx
@@ -1,6 +1,16 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+interface Book {
+    id: string;
+    name: string;
+}
+
 export default function Book() {
+    const { id } = useParams<{ id: string }>();
+    const [book, setBook] = useState<Book | null>(null);
     useEffect(() => {
-        fetch(`http://localhost:4000/api/books/1`) // Appel API Backend
+        fetch(`http://localhost:4000/api/books/${id}`) // Appel API Backend
             .then((response) => response.json())
             .then((data) => setBook(data))
             .catch((error) =>
@@ -8,21 +18,23 @@ export default function Book() {
             );
     }, []);
 
+    console.log("%c⧭", "color: #e50000", book);
+
+    if (!book) return <h1>Chargement...</h1>;
     return (
         <div>
-            <p>je suis le book des images</p>
-            <img
-                src=""
-                alt=""
-            />
-            <img
-                src=""
-                alt=""
-            />
-            <img
-                src=""
-                alt=""
-            />
+            {book.map((picture) => (
+                <div key={picture.picture_id}>
+                    <img
+                        src={picture.path}
+                        alt={picture.picture_name}
+                        width={200}
+                    />
+                    <p>Nom : {picture.picture_name}</p>
+                    <p>Tags : {picture.tags || "Aucun tag"}</p>
+                    <p>chemin : {picture.path}</p>
+                </div>
+            ))}
         </div>
     );
 }
--- server/src/routes/book.ts
+++ server/src/routes/book.ts
@@ -3,32 +3,8 @@ import { getConnection } from "../dbconfig"; // Vérifie que le chemin est bon
 
 const router = express.Router();
 
-// 📌 Route GET pour récupérer un book par ID
-router.get("/books/:id", async (req: Request, res: Response): Promise<void> => {
-  try {
-    const connection = await getConnection(); // Connexion à la BDD
-    const bookId = parseInt(req.params.id, 10);
 
-    // 🔥 Requête SQL pour récupérer un book
-    const [rows]: any = await connection.execute(
-      "SELECT * FROM book WHERE id = ?",
-      [bookId]
-    );
-
-    if (rows.length === 0) {
-      res.status(404).json({ error: "Book non trouvé" });
-      return
-    }
-
-    res.json(rows);
-    return
-  } catch (error) {
-    console.error("❌ Erreur MySQL :", error);
-    res.status(500).json({ error: "Erreur serveur" });
-    return
-  }
-});
-
+// 📌 Route GET pour récupérer tout les books
 router.get("/books", async (req: Request, res: Response): Promise<void> => {
   try {
     const connection = await getConnection(); // Connexion à la BDD
@@ -53,5 +29,48 @@ router.get("/books", async (req: Request, res: Response): Promise<void> => {
     return
   }
 });
+
+// 📌 Route GET pour récupérer un book par ID
+router.get("/books/:id", async (req: Request, res: Response): Promise<void> => {
+  try {
+    const connection = await getConnection(); // Connexion à la BDD
+    const bookId = parseInt(req.params.id, 10);
+
+    // 🔥 Requête SQL pour récupérer un book
+    const [rows]: any = await connection.execute(
+      `SELECT 
+          picture.id AS picture_id, 
+          picture.name AS picture_name, 
+          picture.user_id, 
+          picture.is_private, 
+          picture.create_at, 
+          picture.date_upload,
+          picture.path,
+          GROUP_CONCAT(tag.name) AS tags
+          FROM picture
+          LEFT JOIN picture_tag ON picture_tag.picture_id = picture.id
+          LEFT JOIN tag ON tag.id = picture_tag.tag_id
+          WHERE picture.book_id = ?
+          GROUP BY picture.id;`,
+      [bookId]
+    );
+
+
+
+    if (rows.length === 0) {
+      res.status(404).json({ error: "Book non trouvé" });
+      return
+    }
+
+    res.json(rows);
+    return
+  } catch (error) {
+    console.error("❌ Erreur MySQL :", error);
+    res.status(500).json({ error: "Erreur serveur" });
+    return
+  }
+});
+
+
 
 export default router;
