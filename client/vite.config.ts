7fd533cb1ba53b47ba5c533753214e6e7a48c857
julien44830 <deniaudjulien0@gmail.com>
2025-03-21 13:58:13 +0100

    conf vite for deploye

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,16 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env': process.env
+    "process.env": {} // Évite les erreurs avec `process.env` sur le frontend
   },
   build: {
     outDir: "dist",
   },
   server: {
-    middlewareMode: true,
+    port: 5173, // Définit un port pour éviter des conflits
+    open: true, // Ouvre automatiquement le navigateur
+    proxy: {
+      "/api": {
+        target: process.env.VITE_API_BASE_URL || "http://localhost:4000",
+        changeOrigin: true,
+        secure: false
+      }
+    },
+    historyApiFallback: true, // Permet d'éviter le 404 sur les routes React
   }
-})
+});
