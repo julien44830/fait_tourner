591b3bb7dc0c09c1f636dc54d3f9571138220410
julien44830 <deniaudjulien0@gmail.com>
2025-03-27 17:05:32 +0100

    change env variable on dbconfig

--- server/src/dbconfig.ts
+++ server/src/dbconfig.ts
@@ -1,23 +1,19 @@
 import mysql from 'mysql2/promise';
-import dotenv from 'dotenv';
-
-
-dotenv.config(); // Charge les variables d'environnement
-
 
 export const getConnection = async () => {
     try {
         const connection = await mysql.createConnection({
-            host: process.env.MYSQLHOST,
-            user: process.env.MYSQLUSER,
-            password: process.env.MYSQLPASSWORD,
-            database: process.env.MYSQLDATABASE,
-            port: Number(process.env.MYSQLPORT),
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+            port: Number(process.env.DB_PORT),
         });
 
         console.log("✅ Connexion à la base de données réussie !");
         return connection;
     } catch (err) {
+        console.error("⛔ Erreur lors de la connexion à la base de données :", err);
         throw err;
     }
 };
