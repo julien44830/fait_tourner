83c5e205e04c8dac5fc7d26525900a18c7d5c563
julien44830 <deniaudjulien0@gmail.com>
2025-03-28 19:12:05 +0100

    change port for testing with railway

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,20 @@
+import dotenv from "dotenv";
 import express from "express";
+
+dotenv.config();
 
 const app = express();
 
 console.log("🚨 Démarrage de l'application Express");
+const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 8080;
+console.log("🌍 PORT reçu via process.env:", process.env.PORT);
+
 
 app.get("/", (_req, res) => {
   console.log("✅ Route GET / appelée");
   res.json({ message: "Hello depuis Railway ✅" });
 });
 
-const PORT = parseInt(process.env.PORT || "4000", 10);
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Serveur Express écoute sur le port ${PORT}`);
 });
